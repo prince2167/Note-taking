82f01e807629b21eaae267b61df6a5c80fbe7ac9
Prince <singhprince8222692@gmail.com>
2023-06-05 22:26:30 +0530

    import react icons

---
+++ src/assets/icons.js
@@ -0,0 +1,7 @@
+import { GrHomeRounded } from "react-icons/gr";
+import { MdLabelOutline } from "react-icons/md";
+import { BsArchive } from "react-icons/bs";
+import { CiTrash } from "react-icons/ci";
+import { CgProfile } from "react-icons/cg";
+
+export { GrHomeRounded, MdLabelOutline, BsArchive, CiTrash, CgProfile };
