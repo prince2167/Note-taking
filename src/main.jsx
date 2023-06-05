62511f894142cd94e453854df8574ee407005936
Prince <singhprince8222692@gmail.com>
2023-06-05 22:29:27 +0530

    Add: router

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { NoteProvider } from "./contexts/note-context.jsx";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <NoteProvider>
+        <ToastContainer />
+        <App />
+      </NoteProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
