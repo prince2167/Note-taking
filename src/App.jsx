bace02eb661fbd0aceb53d320d90cf66fdc2b74d
Prince <singhprince8222692@gmail.com>
2023-06-05 22:30:09 +0530

    setup: routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import { Layout } from "./components/index";
+import { Archive, Home, Label, Profile, Trash } from "./Pages/index";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <h1>Note Taking App</h1>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/label" element={<Label />} />
+          <Route path="/archive" element={<Archive />} />
+          <Route path="/trash" element={<Trash />} />
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+      </Routes>
     </>
   );
 }
