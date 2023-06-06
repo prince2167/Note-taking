940b4fe769eafc4cb537e055ce3ef99b299458d8
Prince <singhprince8222692@gmail.com>
2023-06-06 22:50:46 +0530

    add: sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -12,30 +12,33 @@ import { Modal, NoteForm } from "../../components/index";
 
 const Sidebar = () => {
   const [showModal, setShowModal] = useState(false);
+  const getActiveStyle = ({ isActive }) => ({
+    borderRight: isActive ? "4px solid red" : "",
+  });
   return (
     <div className={classes.sidebarContainer}>
       <div className={classes.linksContainer}>
-        <NavLink to="/">
+        <NavLink to="/" style={getActiveStyle}>
           <GrHomeRounded size="25" />
           <p>Home</p>
         </NavLink>
 
-        <NavLink to="/label">
+        <NavLink to="/label" style={getActiveStyle}>
           <MdLabelOutline size="30" />
           <p>Label</p>
         </NavLink>
 
-        <NavLink to="/archive">
+        <NavLink to="/archive" style={getActiveStyle}>
           <BsArchive size="25" />
           <p>Archive</p>
         </NavLink>
 
-        <NavLink to="/trash">
+        <NavLink to="/trash" style={getActiveStyle}>
           <CiTrash size="25" />
           <p>Trash</p>
         </NavLink>
 
-        <NavLink to="/profile">
+        <NavLink to="/profile" style={getActiveStyle}>
           <CgProfile size="25" />
           <p>Profile</p>
         </NavLink>
