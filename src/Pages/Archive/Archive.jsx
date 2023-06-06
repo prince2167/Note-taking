3638b3ade7e76f5c6777251e7ffb3b9132da0a72
Prince <singhprince8222692@gmail.com>
2023-06-06 20:58:56 +0530

    create: archive notes page

--- src/Pages/Archive/Archive.jsx
+++ src/Pages/Archive/Archive.jsx
@@ -1,7 +1,19 @@
+import { NoteCard } from "../../components/index";
+import { useNotes } from "../../contexts/note-context";
 import classes from "./Archive.module.css";
 
 const Archive = () => {
-  return <div>Archive</div>;
+  const { state, dispatch } = useNotes();
+  const { archive } = state;
+  return (
+    <div className={classes.archivePage}>
+      <div className={classes.archiveNotesList}>
+        {archive?.map((note) => (
+          <NoteCard note={note} key={note.id} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Archive;
