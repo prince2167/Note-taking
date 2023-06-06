a6ec5d1d8c2defc977aa3e91464b81455e28a605
Prince <singhprince8222692@gmail.com>
2023-06-06 22:46:39 +0530

    create: trash note list

--- src/Pages/Trash/Trash.jsx
+++ src/Pages/Trash/Trash.jsx
@@ -1,7 +1,19 @@
+import { NoteCard } from "../../components/index";
+import { useNotes } from "../../contexts/note-context";
 import classes from "./Trash.module.css";
 
 const Trash = () => {
-  return <div>Trash</div>;
+  const { state } = useNotes();
+  const { trash } = state;
+  return (
+    <div className={classes.trashPage}>
+      <div className={classes.trashNoteList}>
+        {trash?.map((note) => (
+          <NoteCard note={note} key={note.id} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Trash;
