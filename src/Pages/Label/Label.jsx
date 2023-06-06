be6743a3b97f15a83803042d8b18f90b062cd4a1
Prince <singhprince8222692@gmail.com>
2023-06-06 22:45:00 +0530

    create: label page

--- src/Pages/Label/Label.jsx
+++ src/Pages/Label/Label.jsx
@@ -1,7 +1,35 @@
 import classes from "./Label.module.css";
-
+import { useNotes } from "../../contexts/note-context";
+import { NoteCard } from "../../components/index";
 const Label = () => {
-  return <div>Label</div>;
+  const { state } = useNotes();
+  const { notes } = state;
+
+  const labeledNotes = notes?.filter((note) => note.tag);
+  const tags = [
+    ...new Set(labeledNotes?.map((note) => note?.tag?.toUpperCase())),
+  ];
+
+  return (
+    <div className={classes.labelPage}>
+      <div className={classes.labelContainer}>
+        {tags?.map((tag, index) => (
+          <div key={index}>
+            <h2> {tag} </h2>
+            <div className={classes.labelNotesList}>
+              {labeledNotes
+                ?.filter((note) => note?.tag?.toUpperCase() === tag)
+                .map((note) => (
+                  <>
+                    <NoteCard note={note} key={note.id} />
+                  </>
+                ))}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Label;
