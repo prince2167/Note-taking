bfeb0ba5e50c16e7bfd35e5930746be04e75127e
Prince <singhprince8222692@gmail.com>
2023-06-06 22:43:42 +0530

    create: note list

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,11 @@
 import classes from "./Home.module.css";
-
+import { NoteList } from "../../components/index";
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className={classes.homePage}>
+      <NoteList />
+    </div>
+  );
 };
 
 export default Home;
