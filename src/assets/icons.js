fa36d58c19fdafee5fba18848984e55f657349ff
Prince <singhprince8222692@gmail.com>
2023-06-06 22:47:49 +0530

    import icons

--- src/assets/icons.js
+++ src/assets/icons.js
@@ -1,7 +1,26 @@
 import { GrHomeRounded } from "react-icons/gr";
 import { MdLabelOutline } from "react-icons/md";
-import { BsArchive } from "react-icons/bs";
+import { BsArchive, BsPin, BsPinFill } from "react-icons/bs";
 import { CiTrash } from "react-icons/ci";
 import { CgProfile } from "react-icons/cg";
+import { IoIosColorPalette } from "react-icons/io";
+import { BiArchiveIn, BiArchiveOut } from "react-icons/bi";
+import { RiEditBoxLine } from "react-icons/ri";
+import { AiOutlineDelete } from "react-icons/ai";
+import { FaTrashRestoreAlt } from "react-icons/fa";
 
-export { GrHomeRounded, MdLabelOutline, BsArchive, CiTrash, CgProfile };
+export {
+  GrHomeRounded,
+  MdLabelOutline,
+  BsArchive,
+  CiTrash,
+  CgProfile,
+  BsPin,
+  BsPinFill,
+  IoIosColorPalette,
+  BiArchiveIn,
+  BiArchiveOut,
+  RiEditBoxLine,
+  AiOutlineDelete,
+  FaTrashRestoreAlt,
+};
