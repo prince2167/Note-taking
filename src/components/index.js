282bfbe3d7242abac77c109dac73ffc647720253
Prince <singhprince8222692@gmail.com>
2023-06-06 22:52:19 +0530

    import all components

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,20 @@ import Layout from "./Layout/Layout";
 import Navbar from "./Navbar/Navbar";
 import Modal from "./Modal/Modal";
 import NoteForm from "./NoteForm/NoteForm";
-
-export { Sidebar, Navbar, Layout, Modal, NoteForm };
+import NoteList from "./NoteList/NoteList";
+import NoteCard from "./NoteCard/NoteCard";
+import EditForm from "./EditForm/EditForm";
+import LabelDialouge from "./LabelDialouge/LabelDialouge";
+import ColorPalette from "./ColorPalette/ColorPalette";
+export {
+  Sidebar,
+  Navbar,
+  Layout,
+  Modal,
+  NoteForm,
+  NoteList,
+  NoteCard,
+  EditForm,
+  LabelDialouge,
+  ColorPalette,
+};
