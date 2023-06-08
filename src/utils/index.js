42d62fa114442abeaab167ebe8138985728a67dd
Prince <singhprince8222692@gmail.com>
2023-06-08 23:05:19 +0530

    create: helper function for filters and search

---
+++ src/utils/index.js
@@ -0,0 +1,27 @@
+export const filterNotes = (notes, searchTerm) => {
+  const filteredNotes = notes?.filter(
+    (note) =>
+      note.title.toLowerCase().includes(searchTerm?.toLowerCase()) ||
+      note.tag?.toLowerCase().includes(searchTerm?.toLowerCase())
+  );
+  return filteredNotes;
+};
+
+export const getSortedNotes = (notes, sortBy) => {
+  if ((sortBy = "Newest First")) {
+    const updatedNotes = notes.sort((a, b) => a.currentDate - b.currentDate);
+    return updatedNotes;
+  }
+  if (sortBy === "Oldest First") {
+    const updatedNotes = notes.sort((a, b) => b.currentDate - a.currentDate);
+    return updatedNotes;
+  } else {
+    return notes;
+  }
+};
+
+export const getFilteredNotes = (notes, filters) => {
+  return filters.length > 0
+    ? notes.filter((note) => filters.includes(note.tag))
+    : notes;
+};
