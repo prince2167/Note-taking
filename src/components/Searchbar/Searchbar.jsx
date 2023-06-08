23134207b72511fcb96a070d91cf3a26e1969aa5
Prince <singhprince8222692@gmail.com>
2023-06-08 22:37:17 +0530

    create: serchbar

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,7 +1,37 @@
-import React from "react";
-
+import classes from "./Searchbar.module.css";
+import { AiOutlineSearch, TbAdjustmentsHorizontal } from "../../assets/icons";
+import { useNotes } from "../../contexts/note-context";
+import { Modal, Filter } from "../index";
+import { useState } from "react";
 const Searchbar = () => {
-  return <div>Searchbar</div>;
+  const { searchTerm, setSearchTerm } = useNotes();
+
+  const [showModal, setShowModal] = useState(false);
+  return (
+    <>
+      <div className={classes.container}>
+        <button>
+          <AiOutlineSearch size="22" />
+        </button>
+        <input
+          value={searchTerm}
+          type="text"
+          placeholder="Search..."
+          onChange={(event) => setSearchTerm(event.target.value)}
+        />
+        <button
+          className={classes.filterButton}
+          onClick={() => setShowModal(true)}
+        >
+          <TbAdjustmentsHorizontal size="22" />
+        </button>
+      </div>
+
+      <Modal showModal={showModal} setShowModal={setShowModal}>
+        <Filter setShowModal={setShowModal} />
+      </Modal>
+    </>
+  );
 };
 
 export default Searchbar;
