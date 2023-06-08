a01f674ae16e0fe2b469eb4f62b24ee0cae70af2
Prince <singhprince8222692@gmail.com>
2023-06-08 23:03:25 +0530

    create: state for apply filters

--- src/contexts/note-context.jsx
+++ src/contexts/note-context.jsx
@@ -1,13 +1,37 @@
-import { createContext, useContext, useReducer } from "react";
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useReducer,
+  useState,
+} from "react";
 import { initialState, noteReducer } from "../reducers/noteReducer";
 
 const NoteContext = createContext();
 
-const NoteProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(noteReducer, initialState);
+const storedNote = (initialState) =>
+  JSON.parse(localStorage.getItem("state")) || initialState;
 
+const NoteProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(noteReducer, initialState, storedNote);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [applyFilter, setApplyFilter] = useState(false);
+
+  // local storage
+  useEffect(() => {
+    localStorage.setItem("state", JSON.stringify(state));
+  }, [state]);
   return (
-    <NoteContext.Provider value={{ state, dispatch }}>
+    <NoteContext.Provider
+      value={{
+        state,
+        dispatch,
+        searchTerm,
+        setSearchTerm,
+        applyFilter,
+        setApplyFilter,
+      }}
+    >
       {children}
     </NoteContext.Provider>
   );
