02cb4e33cdbd19cb5a6b9c7f1db84e33f82fe361
Prince <singhprince8222692@gmail.com>
2023-06-09 19:54:46 +0530

    feat: clear archive

--- src/Pages/Archive/Archive.jsx
+++ src/Pages/Archive/Archive.jsx
@@ -1,14 +1,25 @@
+import { toast } from "react-toastify";
 import { NoteCard } from "../../components/index";
 import { useNotes } from "../../contexts/note-context";
+import { filterNotes } from "../../utils";
 import classes from "./Archive.module.css";
 
 const Archive = () => {
-  const { state, dispatch } = useNotes();
+  const { state, searchTerm, dispatch } = useNotes();
   const { archive } = state;
+  const updatedArchiveNotes = filterNotes(archive, searchTerm);
+
+  const clearArchiveHandler = () => {
+    dispatch({ type: "CLEAR_ARCHIVE" });
+    toast.success("Clear archive notes!!");
+  };
   return (
     <div className={classes.archivePage}>
+      {updatedArchiveNotes.length > 0 && (
+        <button onClick={clearArchiveHandler}>Clear</button>
+      )}
       <div className={classes.archiveNotesList}>
-        {archive?.map((note) => (
+        {updatedArchiveNotes?.map((note) => (
           <NoteCard note={note} key={note.id} />
         ))}
       </div>
