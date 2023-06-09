5e9e95de18dd95178051763a71f4818c350373d5
Prince <singhprince8222692@gmail.com>
2023-06-09 20:01:13 +0530

    add: search bar

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,11 +1,18 @@
 import classes from "./Layout.module.css";
 import { Outlet } from "react-router-dom";
 import { Navbar, Searchbar, Sidebar } from "../../components/index";
+import { useState } from "react";
+import { useWindowSize } from "../../hooks/useWindowSize";
 
 const Layout = () => {
+  const [showLinks, setShowLinks] = useState(false);
+  const { width } = useWindowSize();
+  // if (width < 890) {
+  //   setShowLinks(true);
+  // }
   return (
     <>
-      <Navbar />
+      <Navbar showLinks={showLinks} setShowLinks={setShowLinks} />
       <div className={classes.layout}>
         <Sidebar />
         <main className={classes.main}>
