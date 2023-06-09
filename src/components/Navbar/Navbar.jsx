ff42bc944b47de209aa5c18dfbda320fb3ac2871
Prince <singhprince8222692@gmail.com>
2023-06-09 20:05:42 +0530

    make navbar responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,48 @@
+import { useState } from "react";
 import classes from "./Navbar.module.css";
 import { NavLink } from "react-router-dom";
-const Navbar = () => {
+import { Modal, NoteForm } from "../index";
+import {
+  RxHamburgerMenu,
+  CgProfile,
+  SiMicrosoftonenote,
+} from "../../assets/icons";
+import { useWindowSize } from "../../hooks/useWindowSize";
+const Navbar = ({ showLinks, setShowLinks }) => {
+  const [showModal, setShowModal] = useState(false);
+  const { width } = useWindowSize();
   return (
-    <nav>
-      <NavLink to="/" className={classes.logo}>
-        Note-Taking
-      </NavLink>
-    </nav>
+    <>
+      <nav>
+        <div className={classes.logoContainer}>
+          <button onClick={() => setShowLinks(!showLinks)}>
+            <RxHamburgerMenu size="22" />
+          </button>
+          <NavLink to="/" className={classes.logo}>
+            Note-Taking
+          </NavLink>
+        </div>
+
+        <div className={classes.links}>
+          <button
+            className={classes.createNote}
+            onClick={() => setShowModal(true)}
+          >
+            Create new note
+            {/* <SiMicrosoftonenote size="20" /> */}
+          </button>
+          {width < 890 && (
+            <NavLink to="profile">
+              <CgProfile size="20" />
+            </NavLink>
+          )}
+        </div>
+      </nav>
+
+      <Modal showModal={showModal} setShowModal={setShowModal}>
+        <NoteForm setShowModal={setShowModal} />
+      </Modal>
+    </>
   );
 };
 
