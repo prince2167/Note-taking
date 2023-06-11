98271cf4cc493c180b367ce4077f00a655028643
Prince <singhprince8222692@gmail.com>
2023-06-11 22:28:41 +0530

    add: all trash clear button

--- src/Pages/Trash/Trash.jsx
+++ src/Pages/Trash/Trash.jsx
@@ -1,14 +1,24 @@
 import { NoteCard } from "../../components/index";
 import { useNotes } from "../../contexts/note-context";
+import { filterNotes } from "../../utils";
 import classes from "./Trash.module.css";
 
 const Trash = () => {
-  const { state } = useNotes();
+  const { state, searchTerm, dispatch } = useNotes();
   const { trash } = state;
+  const updatedTrash = filterNotes(trash, searchTerm);
+
+  const clearTrashHandler = () => {
+    dispatch({ type: "CLEAR_TRASH" });
+    toast.success("clear trash notes!!");
+  };
   return (
     <div className={classes.trashPage}>
+      {updatedTrash.length > 0 && (
+        <button onClick={clearTrashHandler}>Clear</button>
+      )}
       <div className={classes.trashNoteList}>
-        {trash?.map((note) => (
+        {updatedTrash?.map((note) => (
           <NoteCard note={note} key={note.id} />
         ))}
       </div>
