827d98792cb486df43f75b1ba85c60af70d9afbd
Prince <singhprince8222692@gmail.com>
2023-06-12 12:54:33 +0530

    refactor modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,7 +1,11 @@
 import { createPortal } from "react-dom";
 import classes from "./Modal.module.css";
+import { useEffect } from "react";
 
 const Modal = ({ showModal, setShowModal, children }) => {
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  }, [showModal]);
   return createPortal(
     <>
       {showModal && (
