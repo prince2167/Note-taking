2d0c361c9a0ce8a8986f797e779daad121ef1bc1
Prince <singhprince8222692@gmail.com>
2023-06-12 12:59:04 +0530

    make sidebar responsive

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -16,39 +16,42 @@ const Sidebar = () => {
     borderRight: isActive ? "4px solid red" : "",
   });
   return (
-    <div className={classes.sidebarContainer}>
-      <div className={classes.linksContainer}>
-        <NavLink to="/" style={getActiveStyle}>
-          <GrHomeRounded size="25" />
-          <p>Home</p>
-        </NavLink>
+    <>
+      <div className={classes.sidebarContainer}>
+        <div className={classes.linksContainer}>
+          <NavLink to="/" style={getActiveStyle}>
+            <GrHomeRounded size="25" />
+            <p>Home</p>
+          </NavLink>
 
-        <NavLink to="/label" style={getActiveStyle}>
-          <MdLabelOutline size="30" />
-          <p>Label</p>
-        </NavLink>
+          <NavLink to="/label" style={getActiveStyle}>
+            <MdLabelOutline size="30" />
+            <p>Label</p>
+          </NavLink>
 
-        <NavLink to="/archive" style={getActiveStyle}>
-          <BsArchive size="25" />
-          <p>Archive</p>
-        </NavLink>
+          <NavLink to="/archive" style={getActiveStyle}>
+            <BsArchive size="25" />
+            <p>Archive</p>
+          </NavLink>
 
-        <NavLink to="/trash" style={getActiveStyle}>
-          <CiTrash size="25" />
-          <p>Trash</p>
-        </NavLink>
+          <NavLink to="/trash" style={getActiveStyle}>
+            <CiTrash size="25" />
+            <p>Trash</p>
+          </NavLink>
 
-        <NavLink to="/profile" style={getActiveStyle}>
-          <CgProfile size="25" />
-          <p>Profile</p>
-        </NavLink>
+          <NavLink to="/profile" style={getActiveStyle}>
+            <CgProfile size="25" />
+            <p>Profile</p>
+          </NavLink>
 
-        <button onClick={() => setShowModal(true)}>Create a new note</button>
+          <button onClick={() => setShowModal(true)}>Create a new note</button>
+        </div>
       </div>
+
       <Modal showModal={showModal} setShowModal={setShowModal}>
         <NoteForm setShowModal={setShowModal} />
       </Modal>
-    </div>
+    </>
   );
 };
 
