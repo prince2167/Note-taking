ff5fbc263f1f1aecddb29b910d3abcf22aca0125
Prince <singhprince8222692@gmail.com>
2023-06-12 12:52:48 +0530

    add: responsive navigation links

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,20 +1,22 @@
 import classes from "./Layout.module.css";
 import { Outlet } from "react-router-dom";
-import { Navbar, Searchbar, Sidebar } from "../../components/index";
+import {
+  Navbar,
+  Searchbar,
+  Sidebar,
+  MobileSidebar,
+} from "../../components/index";
 import { useState } from "react";
-import { useWindowSize } from "../../hooks/useWindowSize";
 
 const Layout = () => {
   const [showLinks, setShowLinks] = useState(false);
-  const { width } = useWindowSize();
-  // if (width < 890) {
-  //   setShowLinks(true);
-  // }
+
   return (
     <>
       <Navbar showLinks={showLinks} setShowLinks={setShowLinks} />
       <div className={classes.layout}>
         <Sidebar />
+        {showLinks && <MobileSidebar />}
         <main className={classes.main}>
           <Searchbar />
           <Outlet />
