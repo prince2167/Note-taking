6c1c057b5021d25bcd6d178b73e232b929f62627
Prince <singhprince8222692@gmail.com>
2023-06-14 22:34:12 +0530

    refactor filter from labels

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -6,7 +6,7 @@ import classes from "./Filter.module.css";
 const Filter = ({ setShowModal }) => {
   const { state, dispatch, setApplyFilter } = useNotes();
   const { notes, filters } = state;
-  const labels = notes.map((note) => note.tag);
+  const labels = notes?.map((note) => note.tag);
   const tags = [...new Set(labels)];
 
   const filterByTagsHandler = (event) => {
@@ -32,50 +32,62 @@ const Filter = ({ setShowModal }) => {
   };
 
   return (
-    <div className={classes.filterContainer}>
-      <h2>Short & Filter Notes</h2>
-      <div className={classes.filterTypes}>
-        <div>
-          <strong>Sort</strong>
-          <select
-            onChange={(event) =>
-              dispatch({ type: "SORT_BY", payload: event.target.value })
-            }
-          >
-            {sorts.map((sort, index) => (
-              <option key={index} value={sort}>
-                {sort}
-              </option>
-            ))}
-          </select>
-        </div>
+    <>
+      {notes?.length > 0 && (
+        <div className={classes.filterContainer}>
+          <h2>Short & Filter Notes</h2>
+          <div className={classes.filterTypes}>
+            <div>
+              <strong>Sort</strong>
+              <select
+                onChange={(event) =>
+                  dispatch({ type: "SORT_BY", payload: event.target.value })
+                }
+              >
+                {sorts?.map((sort, index) => (
+                  <option key={index} value={sort}>
+                    {sort}
+                  </option>
+                ))}
+              </select>
+            </div>
 
-        <div className={classes.filterByLabel}>
-          <strong>Select Label</strong>
-          {tags
-            .filter((tag) => tag !== "")
-            .map((tag, index) => (
-              <label key={index} className={classes.filters}>
-                <input
-                  type="checkbox"
-                  value={tag}
-                  checked={filters.includes(tag)}
-                  onChange={filterByTagsHandler}
-                />
-                <p>{tag}</p>
-              </label>
-            ))}
+            {tags?.length > 0 && (
+              <div className={classes.filterByLabel}>
+                <strong>Select Label</strong>
+                {tags
+                  ?.filter((tag) => tag !== "")
+                  .map((tag, index) => (
+                    <label key={index} className={classes.filters}>
+                      <input
+                        type="checkbox"
+                        value={tag}
+                        checked={filters.includes(tag)}
+                        onChange={filterByTagsHandler}
+                      />
+                      <p>{tag}</p>
+                    </label>
+                  ))}
+              </div>
+            )}
+          </div>
+          <div className={classes.buttonGroup}>
+            <button
+              className={classes.clearButton}
+              onClick={clearFilterHandler}
+            >
+              Clear
+            </button>
+            <button
+              className={classes.applyButton}
+              onClick={applyfilterHandler}
+            >
+              Apply
+            </button>
+          </div>
         </div>
-      </div>
-      <div className={classes.buttonGroup}>
-        <button className={classes.clearButton} onClick={clearFilterHandler}>
-          Clear
-        </button>
-        <button className={classes.applyButton} onClick={applyfilterHandler}>
-          Apply
-        </button>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
