d82b276c7d2fabfafcbbab798c7467f5618672e6
Prince <singhprince8222692@gmail.com>
2023-06-14 22:39:10 +0530

    wrap with auth context

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,16 @@ import { BrowserRouter } from "react-router-dom";
 import { NoteProvider } from "./contexts/note-context.jsx";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { AuthProvider } from "./contexts/auth-context.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <NoteProvider>
-        <ToastContainer />
-        <App />
+        <AuthProvider>
+          <ToastContainer />
+          <App />
+        </AuthProvider>
       </NoteProvider>
     </BrowserRouter>
   </React.StrictMode>
