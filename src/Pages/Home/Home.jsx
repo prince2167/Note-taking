b48b31e9a44c96d2ea044dd9dbf921b35b196cb2
Prince <singhprince8222692@gmail.com>
2023-06-14 22:28:24 +0530

    add image

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,15 @@
 import classes from "./Home.module.css";
 import { NoteList } from "../../components/index";
+import { useNotes } from "../../contexts/note-context";
+import img1 from "../../assets/images/notes.gif"
 const Home = () => {
+  const { state } = useNotes();
+  const { notes } = state;
   return (
     <div className={classes.homePage}>
+      {notes?.length === 0 && (
+        <img src={img1} alt="" />
+      )}
       <NoteList />
     </div>
   );
